c7b19acdd727855068418369cbc5194a206f375d
Hikmaalakbaar <rohiderboy24@gmail.com>
2025-07-17 20:17:27 +0700

    Penambahan colorama

--- toDoList.py
+++ toDoList.py
@@ -1,9 +1,14 @@
 import os
+from datetime import datetime
+from colorama import init, Fore, Style
+
+#Inisialisasi colorama
+init(autoreset=True)
 
 #file untuk menyimpan data
 FILE_TODO = 'todo.txt'
 
-#fungsi untuk load data dari file
+#fungsi untuk memuat daftar tugas dari file
 def load_todos():
     if not os.path.exists(FILE_TODO):
         return[]
@@ -19,7 +24,7 @@ def save_todos(todos):
 
 # Menampilkan menu utama
 def show_menu():
-    print("\n=== TO-DO LIST ===")
+    print( Fore.CYAN + "\n=== TO-DO LIST ===")
     print("1. Tambah Tugas")
     print("2. Lihat Semua Tugas")
     print("3. Tandai Tugas Selesai")
@@ -28,56 +33,70 @@ def show_menu():
 
 # Menjalankan program
 def main():
-    todos = load_todos()
+    todos = load_todos() #Load data saat program pertama dijalankan
+
     while True:
         show_menu()
         choice = input('Pilih menu: ')
 
         if choice == '1':
+            # Tambah tugas baru
             task = input('Masukkan nama tugas: ')
-            todos.append(f'[ ] {task}')
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            todos.append(f'[ ] {task} (ditambahkan pada {timestamp})')
             save_todos(todos)
-            print('Tugas Berhasil ditambahkan.')
+            print( Fore.GREEN + 'Tugas Berhasil ditambahkan.')
 
         elif choice == '2':
-            print('\n=== Daftar Tugas ===')
+            #Lihat semua tugas
+            print( Fore.YELLOW +'\n=== Daftar Tugas ===')
             if not todos:
                 print('Belum ada tugas.')
             else:
                 for i, task in enumerate(todos, 1):
-                    print(f"{i}. {task}")
+                    color = Fore.RED if "[x]" in task else Fore.WHITE
+                    print(color + f"{i}. {task}")
         
         elif choice == '3':
+            # Tandai tugas selesai
             if not todos:
                 print('Tidak ada tugas untuk ditandai.')
                 continue
             for i, task in enumerate(todos, 1):
                 print(f'{i}. {task}')
+            try:
                 index = int(input('Masukan nomor tugas yang selesai: ')) - 1
-                if  todos[index].startswith('[ ]'):
-                    todos[index] = todos[index].replace('[]', '[x]', 1)
+                if  "[ ]" in todos[index]:  
+                    todos[index] = todos[index].replace('[ ]', '[x]', 1)
                     save_todos(todos)
-                    print('Tugas ditandai sebagai selesai.')
-            else:
-                print('Tugas sudah selesai sebelumnya.')
+                    print( Fore.GREEN + 'Tugas ditandai sebagai selesai.')
+                else:
+                    print(Fore.YELLOW + 'Tugas sudah selesai sebelumnya.')
+            except:
+                print(Fore.RED + "Input Tidak Valid.")
 
         elif choice == '4':
+            #Hapus Tugas
             if not todos:
                 print('Tidak ada tugas untuk dihapus.')
                 continue
             for i, task in enumerate(todos, 1):
                 print(f'{i}. {task}')
-            index = int(input('Masukkan nomor tugas yang ingin dihapus: ')) - 1
-            deleted = todos.pop(index)
-            save_todos(todos)
-            print(f'Tugas "{deleted}" berhasil dihapus.')
+            try:
+                index = int(input('Masukkan nomor tugas yang ingin dihapus: ')) - 1
+                deleted = todos.pop(index)
+                save_todos(todos)
+                print(Fore.GREEN + f'Tugas "{deleted}" berhasil dihapus.')
+            except:
+                print(Fore.RED + "Input Tidak Valid atau Nomor salah.")
 
         elif choice == '5':
-            print('Terima kasih telah menggunakan To-Do List!')
+            #Keluar dari program
+            print(Fore.CYAN + 'Terima kasih telah menggunakan To-Do List!')
             break
 
         else:
-            print('Pilihan tidak valid. Silakan coba lagi.')
+            print(Fore.RED + 'Pilihan tidak valid. Silakan coba lagi.')
 
 if __name__ == '__main__':
     main()
